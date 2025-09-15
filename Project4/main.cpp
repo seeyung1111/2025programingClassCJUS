7a25fb357bcc82465c524d3a46fd32a169662faa
seeyung1111 <isiyeongnam81@gmail.com>
2025-09-15 16:43:56 +0900

    project4 finish

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -2,10 +2,10 @@
 int  main()
 {
 	//=== Basic Constants ===
-	printf("Name   : Seeyung\n");
-		printf("Initial : S\n");
-		printf("Age : 21\n");
-		printf("Score : 93.500000\n");
-		printf("count : 3000000000\n");
+	printf("Name   : Seeyung\n\n");
+		printf("Initial : S\n\n");
+		printf("Age : 21\n\n");
+		printf("Score : 93.500000\n\n");
+		printf("count : 3000000000\n\n");
 		return 0;
 }
