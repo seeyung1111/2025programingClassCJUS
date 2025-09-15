01e5a9d0fa39d14246463359a6d18ce3715aff76
seeyung1111 <isiyeongnam81@gmail.com>
2025-09-15 16:10:09 +0900

    ghjgjh

---
+++ Project4/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int  main()
+{
+	//=== Basic Constants ===
+	printf("Name   : Seeyung");
+		printf("Initial : S");
+		printf   ("Age : 21");
+		printf   ("Score : 93.500000");
+		printf   ("count : 3000000000");
+		return 0;
+}
