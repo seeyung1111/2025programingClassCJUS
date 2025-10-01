72461bebd5555607cdd26a543c2718125e30c51c
seeyung1111 <isiyeongnam81@gmail.com>
2025-10-01 09:50:01 +0900

    Project8
    seeyung1111

---
+++ Project8/main.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main(void)
+{
+	// 1: 변수 선언(이름, 학번, 키, 몸무게, BMI)
+	// 2: scanf 또는 scanf_s로 위 정보를 한 줄로 입력 받기
+	// 3: 간단한 BMI 계산(BMI = 키 * 몸무게)
+	// 4-1: 결과츨력(이름, 학번, 키, 몸무게)
+	// 4-2: 결과출력(BMI)
+	return 0;
+}
